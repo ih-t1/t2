8bc5d67570422e28e454f1ede7f5e0c4cfcad96b
ih-t1 <luis.junco+t1@ironhack.com>
2024-12-10 12:33:01 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+import 'dotenv/config';
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+import express from "express";
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+import configMiddleware from "./config";
+configMiddleware(app);
+
+
+// 👇 Start handling routes here
+import indexRoutes from "./routes/index.routes";
+app.use("/api", indexRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+import errorHandling from "./error-handling";
+errorHandling(app);
+
+export default app;
